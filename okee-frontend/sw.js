9fbb9b84ea9239dd939c64d89e541929cde52d43
Michael Perez <your-email@example.com>
2026-03-09 12:30:11 -0400

    Adding icons, preparing for deployment, cloudfront and cognito connected.

--- okee-frontend/sw.js
+++ okee-frontend/sw.js
@@ -7,6 +7,8 @@ const STATIC_ASSETS = [
     './manifest.json',
     './okee-map.jpg',
     './venue-2026.jpg',
+    './icons/icon-192.png',
+    './icons/icon-512.png',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
